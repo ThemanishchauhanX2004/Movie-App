8c7218971646683af3004a9869e4b4019f2aab6c
manish chauhan <manishchauhan7783@gmail.com>
2025-11-09 03:12:10 +0530

    updating

--- movie-app/src/api/apiConfig.js
+++ movie-app/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "086cfe05dd16828e37291d2f37293a38",
+  apiKey: import.meta.env.VITE_TMDB_KEY, // Vite me ye sahi hai
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
